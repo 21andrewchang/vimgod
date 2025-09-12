bcf26f4febc734f73f9cac28446de4a1812c5028
nicolu0 <nicoluo@gmail.com>
2025-09-12 15:03:09 -0500

    fix: global packages and env file

--- app/src/lib/supabaseClient.ts
+++ app/src/lib/supabaseClient.ts
@@ -1,5 +1,5 @@
 import { createClient } from '@supabase/supabase-js';
 import { PUBLIC_SUPABASE_URL, PUBLIC_SUPABASE_ANON_KEY } from '$env/static/public';
-console.log('env: ', import.meta.env.PUBLIC_SUPABASEURL);
+console.log('env: ', import.meta.env.PUBLIC_SUPABASE_URL);
 
 export const supabase = createClient(PUBLIC_SUPABASE_URL, PUBLIC_SUPABASE_ANON_KEY);
