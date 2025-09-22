85a2b654b28721e463cb39d7387e91bc8d9f84cf
Andrew Chang <21andrewch@gmail.com>
2025-09-22 14:42:23 -0500

    feat: cursor blink target

--- app/src/lib/components/Singularity.svelte
+++ app/src/lib/components/Singularity.svelte
@@ -1,19 +1,28 @@
 <script>
-	import AnimatedGradientText from './AnimatedGradientText.svelte';
 	import { cn } from '$lib/utils';
 </script>
 
 <div class="z-10 flex items-center justify-center">
-	<AnimatedGradientText>
+	<div
+		class="relative inline-flex overflow-hidden rounded-full p-px shadow-[0_0px_20px_rgba(255,0,0,0.25)] shadow-[0_0px_20px_rgba(255,0,255,0.25)]"
+	>
 		<span
-			class={cn(
-				'animate-gradient inline rounded-xl bg-gradient-to-r from-[#ffaa40] via-[#9c40ff] to-[#ffaa40] bg-clip-text px-3 py-1 font-mono text-sm text-transparent'
-			)}
-			style="--bg-size:200%; background-size: var(--bg-size) 100%;"
+			class="absolute inset-[-1000%] animate-[spin_4s_linear_infinite] bg-[conic-gradient(from_90deg_at_50%_50%,#C9E0FD_0%,#A9CCFA_18%,#84B7EB_36%,#000000_54%,#000000_72%,#C9E0FD_100%)] opacity-80
+         will-change-[transform,background-position]"
+		/>
+		<span
+			class="inline-flex h-full w-full items-center justify-center rounded-full bg-neutral-950 px-3 py-1 text-xs backdrop-blur-3xl"
 		>
-			Singularity
+			<span
+				class={cn(
+					'animate-gradient inline bg-gradient-to-r from-[#EEE] via-[#C9E0FD] to-[#EEE] bg-clip-text  font-mono text-sm text-transparent'
+				)}
+				style="--bg-size:200%; background-size: var(--bg-size) 100%;"
+			>
+				Singularity
+			</span>
 		</span>
-	</AnimatedGradientText>
+	</div>
 </div>
 
 <style>
